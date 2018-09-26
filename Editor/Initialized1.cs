1fd29027eba269eeda5dec514e699afa5e3ed920
SuzuSho130 <shibaura-it.ac.jp>
2018-09-27 00:56:13 +0900

    Second commit

---
+++ Editor/Initialized1.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Media;
+
+[InitializeOnLoad]
+class Initialized1
+{
+	private static SoundPlayer player;
+	private static bool flag = true;
+	static Initialized1 ()
+	{
+		player = new SoundPlayer ("Assets/Editor/Unity.wav");
+		//EditorApplication.update += Update;
+	}
+
+	static void Update ()
+	{
+		if (EditorApplication.isCompiling) {
+			if (flag) {
+				player.PlaySync ();
+				flag = false;
+			}
+		} else {
+			flag = true;
+		}
+	}
+}
